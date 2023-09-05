e9aa0ade3e0df37f2b2e97a40d45afb943e54119
Tony Wang <wangh9947@gmail.com>
2023-09-05 13:47:38 +0800

    initial Tasks display with color and checkbox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [userInput, setUserInput] = useState("");
+  const initialTasks = [
+    { text: "do work", isComplete: false, color: "red" },
+    { text: "play fifa", isComplete: true, color: "pink" },
+    { text: "do housework", isComplete: false, color: "black" },
+    { text: "play sports", isComplete: true, color: "green" },
+  ];
+  const [tasks, setTasks] = useState(initialTasks);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To Do List</h1>
+      <input
+        onChange={(e) => {
+          setUserInput(e.target.value);
+        }}
+      ></input>
+      <button>Add Task</button>
+
+      {tasks.map((e) => {
+        return (
+          <div style={{ color: e.color }}>
+            {e.text}
+            <input type="checkbox" checked={e.isComplete} />
+          </div>
+        );
+      })}
     </div>
   );
 }
